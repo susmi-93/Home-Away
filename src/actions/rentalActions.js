e1aacc85ec12a8345daa0f4ca8859bfae8a1be71
susmitha <susmithahnair@gmail.com>
2019-10-30 14:54:40 -0400

    Switching to https URL

--- src/actions/rentalActions.js
+++ src/actions/rentalActions.js
@@ -1,7 +1,7 @@
 import { FETCH_RENTAL, ADD_RENTAL } from "./types";
 
 export const fetchRentals = () => dispatch => {
-  fetch("http://renter.getsandbox.com/homes/shortlisted")
+  fetch("https://renter.getsandbox.com/homes/shortlisted")
     .then(res => res.json())
     .then(rentals =>
       dispatch({
